9d9a6d39df6144403af22af8b1eeb9fe4b72a65d
dizzysch <victorwilbert@gmail.com>
2023-09-26 17:41:16 -0300

    separa serializacao e desserializacao

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -8,6 +8,19 @@ namespace ExemploExplorando.Models
     [JsonProperty("Nome_Produto")]
     public string Produto { get; set; }
     public decimal Preco { get; set; }
+    [JsonProperty("DataVenda")]
     public DateTime DataDaVenda { get; set; }
+
+    public void LeArquivo()
+    {
+      string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+      List<Venda> listaVendas = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+      foreach (Venda venda in listaVendas)
+      {
+        Console.WriteLine($"Id: {venda.Id}, produto: {venda.Produto}, preco: {venda.Preco}, Data: {venda.DataDaVenda}");
+        Thread.Sleep(500);
+      }
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,2 @@
 ﻿using ExemploExplorando.Models;
-using Newtonsoft.Json;
 
-string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
-List<Venda> listaVendas = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
-
-foreach (Venda venda in listaVendas)
-{
-  Console.WriteLine($"Id: {venda.Id}, produto: {venda.Produto}, preco: {venda.Preco}, Data: {venda.DataDaVenda}");
-  Thread.Sleep(500);
-}
